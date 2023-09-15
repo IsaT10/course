402def8f20e9100cc65e7384de01c8c658f46152
IsaT <rakiib010@gmail.com>
2023-09-15 06:05:59 +0600

    fake data added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import SingleCard from "../SingleCard/SingleCard";
+
 const Cards = () => {
-  return <div className="md:col-span-2 lg:col-span-3">Card</div>;
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("data.json");
+        const data = await response.json();
+        setCourses(data);
+        console.log("Fetched data:", data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="md:col-span-2 lg:col-span-3">
+      {courses.map((course) => (
+        <SingleCard key={course.id} course={course} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/SingleCard/SingleCard.jsx
+++ src/components/SingleCard/SingleCard.jsx
@@ -1,5 +1,13 @@
-const SingleCard = () => {
-  return <div> SingleCard</div>;
+const SingleCard = ({ course }) => {
+  const { id, cover_photo, title, hours, description, price } = course;
+  return (
+    <div className="bg-white ">
+      {" "}
+      <img src={cover_photo} alt="" />
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 export default SingleCard;
